9f09194243fe470cac1f22552e34761cbc14d792
dhayanithik <85940175+dhayanithik@users.noreply.github.com>
2022-01-29 21:08:06 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -6,6 +6,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
+    await m.reply(f'Hi {m.from_user.mention(style="md")},Hello ㅤ,
+
+I'm A simple link Generator Bot !💯.
+
+Send me any TELEGRAM file, I'll generate instant stream/download link for you!
+
+NB: You Need To Join Our Channel to Use This BoT..
+
+©️Powered By @File_to_online_link .',
                 
                   )
